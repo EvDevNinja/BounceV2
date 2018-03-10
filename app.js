e0768f0ac634c180a03838a3fb5fa9796f37b5ea
EvDevNinja <everettapps@gmail.com>
2018-03-10 16:35:36 -0600

    Added External Config

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const cluster = require('cluster');
 const net = require('net');
 const numCPUs = require('os').cpus().length;
 
-const config = {
-	"8080":{//External Port
-		"aiden.evdev.online":{//Filter
-			"internal":"localhost",//Internal Address
-			"port":80 //Internal Port
-		}
-	}
+if (!fs.existsSync('./config.json')) {
+	fs.writeFileSync('./config.json', '{\n\t"8080":{\n\t\t"aiden.evdev.online":{\n\t\t\t"internal":"localhost",\n\t\t\t"port":80\n\t\t}\n\t}\n}', 'utf8')
 }
+const config = require('./config.json')
 
 if(cluster.isMaster){
    console.log(`Master ${process.pid} is running`);
@@ -27,41 +23,43 @@ if(cluster.isMaster){
   for (externalPortStr in config) {
     var externalPort = Int(externalPortStr); //Parse Int Optional
     if (externalPort == undefined) { continue; } //Skip
-    var server = net.createServer((socket) => {
-      socket.once('data', (buffer) => {
-        for (var search in config[externalPortStr]) {
-          if (buffer.includes(search) || search == "*") {
-            var client = net.createConnection(config[externalPortStr][search].port, config[externalPortStr][search].internal, () => {
-              socket.write(buffer);
-            });
-            client.on('data', (dat) => {
-              socket.write(dat);
-            });
-            client.on('error', (err) => {
-              console.log(err);
-            });
-            client.on('end', () => {
-              socket.end();
-            });
-            socket.on('data', (dat) => {
-              client.write(dat);
-            });
-            socket.on('end', () => {
-              client.end()
-            });
-            return;
-          }
-        }
-        socket.end();
-      });
-      socket.on('error', (err) => {
-        console.log(err);
-      });
-    }).on('error', (err) => {
-      console.log(err);
-    }).listen(externalPort, () => {
-      console.log(`Worker ${process.pid} listening on port ${externalPort}`);
-    });
+		(function(externalPort){
+			var server = net.createServer((socket) => {
+	      socket.once('data', (buffer) => {
+	        for (var search in config[externalPortStr]) {
+	          if (buffer.includes(search) || search == "*") {
+	            var client = net.createConnection(config[externalPortStr][search].port, config[externalPortStr][search].internal, () => {
+	              socket.write(buffer);
+	            });
+	            client.on('data', (dat) => {
+	              socket.write(dat);
+	            });
+	            client.on('error', (err) => {
+	              console.log(err);
+	            });
+	            client.on('end', () => {
+	              socket.end();
+	            });
+	            socket.on('data', (dat) => {
+	              client.write(dat);
+	            });
+	            socket.on('end', () => {
+	              client.end()
+	            });
+	            return;
+	          }
+	        }
+	        socket.end();
+	      });
+	      socket.on('error', (err) => {
+	        console.log(err);
+	      });
+	    }).on('error', (err) => {
+	      console.log(err);
+	    }).listen(externalPort, () => {
+	      console.log(`Worker ${process.pid} listening on port ${externalPort}`);
+	    });
+		})(externalPort)
   }
   console.log(`Worker ${process.pid} started`);
 }
